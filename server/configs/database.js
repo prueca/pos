266b01278d51c197784b56c251c053082485e3f2
Peter John Rueca <peterrueca@yahoo.com>
2020-06-13 22:13:33 +0800

    updated db config

--- server/configs/database.js
+++ server/configs/database.js
@@ -1,9 +1,9 @@
 import { env } from '../library/helper';
 
 export default {
-  DATABASE: env('DATABASE', 'heroku_06c4a8a6445d25e'),
-  USER: env('USER', 'b8d988047b57f3'),
-  PASS: env('PASS', '548b6252'),
+  DATABASE: env('DATABASE', 'heroku_6794ced17224118'),
+  USER: env('USER', 'bcfc0f80768388'),
+  PASS: env('PASS', '0d4d4716'),
   HOST: env('HOST', 'us-cdbr-east-05.cleardb.net'),
   PORT: env('PORT', 3306),
   DIALECT: env('DIALECT', 'mysql'),
