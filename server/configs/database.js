497d6c8ebd2189195f2bae5f1ba7b3eff02281b2
Peter John Rueca <peterrueca@yahoo.com>
2020-06-20 07:56:57 +0800

    fixed db connection

--- server/configs/database.js
+++ server/configs/database.js
@@ -1,12 +1,12 @@
 import { env } from '../library/helper';
 
 export default {
-  DATABASE: env('DATABASE', 'heroku_6794ced17224118'),
-  USER: env('USER', 'bcfc0f80768388'),
-  PASS: env('PASS', '0d4d4716'),
-  HOST: env('HOST', 'us-cdbr-east-05.cleardb.net'),
-  PORT: env('PORT', 3306),
-  DIALECT: env('DIALECT', 'mysql'),
+  DATABASE: env('DB_NAME', 'heroku_6794ced17224118'),
+  USER: env('DB_USER', 'bcfc0f80768388'),
+  PASS: env('DB_PASS', '0d4d4716'),
+  HOST: env('DB_HOST', 'us-cdbr-east-05.cleardb.net'),
+  PORT: env('DB_PORT', 3306),
+  DIALECT: env('DB_DIALECT', 'mysql'),
   LOGGING: false, // console.log
   FORCE_SYNC: false
 };
