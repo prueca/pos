16b2cd25f43c7e2df059fc0105588d4124d2496d
Peter John Rueca <peterrueca@yahoo.com>
2020-06-20 09:11:40 +0800

    moved loading of .env to package.json

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 module.exports = {
   babelrc: true,
   mode: 'universal',
--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-import 'dotenv/config';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import { ready } from 'consola';
